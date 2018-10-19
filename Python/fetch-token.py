e919fc83041464ac0e651b8f548e9b28b0f5f734
Reid Stidolph <rstidolph@rainier.reidlab.com>
2018-10-19 14:48:39 -0700

    Clean up and comments added

--- Python/fetch-token.py
+++ Python/fetch-token.py
@@ -1,14 +1,31 @@
-import random, requests, json, pprint, sseclient
+#!/usr/bin/env python3
+
+# Import modules
+import requests, json
+
+# Disable warning thrown when requests are made to 128T hosts using default self-signed cert)
+# DO NOT USE IN PRODUCTION APP!
+# Instead it is reccomended to use a trusted cert on your 128T, and omit this line
+requests.packages.urllib3.disable_warnings()
 
 # Retrieving Authentication Token
-
-url = "<Your_URL>"
+# URL for retrieving token. Update with your host address or FQDN
+url = "https://<your-128T-host>/api/v1/login"
 payload = {
-    'username': '<Username>',
-    'password': '<Password>'
+  'username': '<Username>',
+  'password': '<Password>'
 }
 
-header1 = {'content-type': 'application/json'}
-response = requests.post(url, data=json.dumps(payload), headers = header1, verify = False)
+# Create token request
+response = requests.post(
+  url,
+  data=json.dumps(payload),
+  headers = {'content-type': 'application/json'},
+  # Do not verify that SSL certificate provided by 128T host is trusted
+  # DO NOT USE IN PRODUCTION APP!
+  # Instead it is reccomended to use a trusted cert on your 128T, and omit this line
+  verify = False
+)
 
+# Print results of token request
 print(response.text)
