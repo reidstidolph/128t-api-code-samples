e5a8503dc422e137a071d4497ce630a5a0d07191
Reid Stidolph <rstidolph@128technology.com>
2018-10-20 17:18:44 -0700

    added error handling

--- Python/get-kpi.py
+++ Python/get-kpi.py
@@ -3,11 +3,11 @@
 # This retrieves key performance metrics for a given router node.
 
 # Import modules
-import requests, json
+import sys, requests, json
 
 # Disable warning thrown when requests are made to 128T using self-signed cert.
 # DO NOT USE IN PRODUCTION APP!
-# Instead it is reccomended to use a trusted cert on your 128T, and omit this.
+# Instead it is recommended to use a trusted cert on 128T, and omit this.
 requests.packages.urllib3.disable_warnings()
 
 # Valid auth token string
@@ -17,18 +17,30 @@ url = "https://<128T-HOST>/api/v1/router/<ROUTER-NAME>/node/<NODE-NAME>"
 # Request headers
 requestHeaders = {'Authorization': 'Bearer {}'.format(token)}
 
-# Create node kpi request
-response = requests.get(
-  url,
-  headers = requestHeaders,
-  # Do not verify that SSL certificate provided by 128T host is trusted
-  # DO NOT USE IN PRODUCTION APP!
-  # Instead it is reccomended to use a trusted cert on your 128T, and omit this
-  verify = False
-)
+# Try reqeust to 128T host
+try:
+  response = requests.get(
+    url,
+    headers = requestHeaders,
+    # Do not verify that SSL certificate provided by 128T host is trusted
+    # DO NOT USE IN PRODUCTION APP!
+    # Instead it is recommended to use a trusted cert on 128T, and omit this
+    verify = False
+  )
+# Handle errors connecting to 128T host
+except requests.exceptions.RequestException as error:
+  print(error)
+  sys.exit(1)
 
-# Parse response payload into JSON object
-responsePayload = json.loads(response.text)
+# Handle success response from 128T host
+if response.status_code == 200:
+  # Parse response payload into JSON object
+  responsePayload = json.loads(response.text)
+  # Print results of node info request (using json.dumps for pretty formatting)
+  print(json.dumps(responsePayload, sort_keys=False, indent=2))
 
-# Print results of node info request (using json.dumps for pretty formatting)
-print(json.dumps(responsePayload, sort_keys=False, indent=2))
+# Handle error response from 128T host
+else:
+  print('Error code: ' + str(response.status_code))
+  print(response.text)
+  sys.exit(1)
